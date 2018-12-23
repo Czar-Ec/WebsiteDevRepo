1609909774bb72d82edc9563be5fa291831ad8cd
CzarEc <czar.echavez@yahoo.co.uk>
2018-12-23 09:06:20 +0000

    Dev: Dark theme and light theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  setDarkTheme = true;
+
   title = 'WebsiteDevRepo';
+
+  toggleDarkTheme() {
+    this.setDarkTheme = !this.setDarkTheme;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './modules/app-routing.module';
 import { AppComponent } from './app.component';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { MaterialModule } from './material.module';
+import { MaterialModule } from './modules/material.module';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { ContentComponent } from './components/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
